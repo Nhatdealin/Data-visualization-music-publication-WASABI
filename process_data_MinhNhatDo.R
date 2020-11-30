4664e32dbcb23e7358b46da87997667dacb4b75f
Nhatdealin <dominhnhat2311@gmail.com>
2020-11-30 02:09:02 +0100

    import songs and merge data with albums

--- process_data_MinhNhatDo.R
+++ process_data_MinhNhatDo.R
@@ -1,15 +1,23 @@
 library(readr)
 library(dplyr)
+library(data.table)
+library(ggplot2)
 
 
 setwd("~/Workspaces/DSAI/Data-visualization-MDSAI")
-
-albums <- read_csv('data/wasabi_albums.csv')
+f <- function(x, pos){
+  subset(x,select=c())
+  return(x %>% select(c()))
+}
+albums <- fread('data/wasabi_albums.csv', select=c("_id", "genre", "id_artist", "length", "name", "country"))
 artists <- read_csv('data/wasabi_artists.csv')
+songs <- fread('data/wasabi_songs.csv', header=TRUE, 
+               select=c("_id", "id_album", "name", "language", "publicationDateAlbum"), 
+               verbose=TRUE, blank.lines.skip=T)
 head(album$dateRelease)
 colnames(artists)
 filter_albums <- albums %>% 
-  select(c("_id", "genre", "id_artist", "length", "name", "publicationDate")) %>%
+  select() %>%
   filter(publicationDate == 1997 && !is.null(`_id`))
 
 
@@ -18,5 +26,13 @@ filter_artists <- artists %>%
   filter(!is.null(`_id`)) 
 
 names(filter_artists)[3] = "artist_name"
-new_data <- filter_albums %>% left_join(filter_artists, by=c("id_artist"="_id"))
+new_data <- songs %>% left_join(albums, by=c("id_album"="_id"))
 summary(new_data)
+
+new_data$publicationDateAlbum <- as.numeric(new_data$publicationDateAlbum)
+data <- new_data %>% 
+  filter(country != "") %>% 
+  group_by(country,publicationDateAlbum) %>% 
+  summarise(count = n_distinct(`_id`))
+data
+data %>% filter(count > 50) %>% ggplot() + geom_point(aes(x=publicationDateAlbum, y=count, col=country)) + ggtitle("Plot scatter date and count song publication")
