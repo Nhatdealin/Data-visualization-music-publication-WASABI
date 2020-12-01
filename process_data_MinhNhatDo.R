b81e3ec14f76df5d6c4783b0a1c99657c6332e06
Nhatdealin <dominhnhat2311@gmail.com>
2020-12-01 01:02:16 +0100

    export new data with name country

--- process_data_MinhNhatDo.R
+++ process_data_MinhNhatDo.R
@@ -9,6 +9,7 @@ f <- function(x, pos){
   subset(x,select=c())
   return(x %>% select(c()))
 }
+country <- read_csv('data/country.csv')
 albums <- fread('data/wasabi_albums.csv', select=c("_id", "genre", "id_artist", "length", "name", "country"))
 artists <- read_csv('data/wasabi_artists.csv')
 songs <- fread('data/wasabi_songs.csv', header=TRUE, 
@@ -16,23 +17,24 @@ songs <- fread('data/wasabi_songs.csv', header=TRUE,
                verbose=TRUE, blank.lines.skip=T)
 head(album$dateRelease)
 colnames(artists)
-filter_albums <- albums %>% 
-  select() %>%
-  filter(publicationDate == 1997 && !is.null(`_id`))
-
-
 filter_artists <- artists %>% 
   select(c("_id", "genres", "name", "location.country", "gender")) %>%
   filter(!is.null(`_id`)) 
 
 names(filter_artists)[3] = "artist_name"
-new_data <- songs %>% left_join(albums, by=c("id_album"="_id"))
+albums <- albums %>% 
+  filter(country != "") %>% 
+  left_join(country, by=c("country"="Code")) %>%
+  select(-country) %>%
+  rename(country=Name) %>% filter(!is.na(country))
+new_data <- songs %>% inner_join(albums, by=c("id_album"="_id"))
 summary(new_data)
 
 new_data$publicationDateAlbum <- as.numeric(new_data$publicationDateAlbum)
+new_data <- filter(new_data, !is.na(publicationDateAlbum))
 data <- new_data %>% 
-  filter(country != "") %>% 
   group_by(country,publicationDateAlbum) %>% 
   summarise(count = n_distinct(`_id`))
 data
-data %>% filter(count > 50) %>% ggplot() + geom_point(aes(x=publicationDateAlbum, y=count, col=country)) + ggtitle("Plot scatter date and count song publication")
+write.csv(data, "./song_count_by_country_date.csv", row.names=FALSE)
+data %>% ggplot() + geom_point(aes(x=publicationDateAlbum, y=count, col=country)) + ggtitle("Plot scatter date and count song publication")
